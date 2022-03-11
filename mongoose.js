e3125a37095e8bc7a363c2274aa4c2b0b5956db4
Osama <batch17.026@gmail.com>
2022-03-11 10:46:57 +0500

    crud operations using mongoose

--- mongoose.js
+++ mongoose.js
@@ -1,20 +1,74 @@
 const mongoose=require('mongoose');
 
 
-const main=async()=>{
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  price: String,
+});
 
-  await mongoose.connect("mongodb+srv://Osama:rise$123@cluster.s2xkc.mongodb.net/ecommerce");
 
-  const ProductSchema=new mongoose.Schema({
-      name:String,
-      price:String
+/* 
+ const main = async () => {
+  let data = new ProductModel({
+    name: "Iphone 12",
+    price: 6753,
   });
-
-  const ProductModel=mongoose.model('products',ProductSchema);
-  let data=new ProductModel({name:"Iphone 13",price:"$1983"});
-  let res=await data.save();
+  let res = await data.save();
   console.log(res);
+};
+
+main();   */
+
+
+//Updating database:
+/* const updateinDB = async () => {
+
+  await mongoose.connect(
+    "mongodb+srv://Osama:rise$123@cluster.s2xkc.mongodb.net/ecommerce"
+  );
+
+  const ProductModel = mongoose.model("products", ProductSchema);
+  let data = await ProductModel.updateOne(
+    { name: "m5" },
+    {
+      $set: {
+        price: "$750",
+        name:"P4",
+      },
+    }
+  );
+
+  console.log(data);
+};
+
+updateinDB(); */
+
+//Delete in Mongodb:
+
+/*
+ const deleteinDB=async()=>{
+  await mongoose.connect(
+    "mongodb+srv://Osama:rise$123@cluster.s2xkc.mongodb.net/ecommerce"
+  );
+
+  const ProductModel = mongoose.model("products", ProductSchema);
+  let data=await ProductModel.deleteOne( {name:"P4"});
+  console.log(data);
 
 }
 
-main();
+deleteinDB(); */
+
+
+
+const findinDB=async()=>{
+  await mongoose.connect(
+    "mongodb+srv://Osama:rise$123@cluster.s2xkc.mongodb.net/ecommerce"
+  );
+
+  const ProductModel = mongoose.model("products", ProductSchema);
+  let data=await ProductModel.find();
+  console.log(data);
+
+}
+findinDB();
